b0f0a73a3f91674116d5059e993649bd95366490
Abdelkader Elsisy <66458422+abdelkader470@users.noreply.github.com>
2023-11-21 00:56:31 +0200

    create server

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config({ path: "config.env" });
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const dbConnection = require("./config/database");
+const categoryRoute = require("./routes/categoryRoute");
+const app = express();
+
+// Database Connection
+dbConnection();
+//Middleware
+app.use(express.json());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+  console.log(`mode: ${process.env.NODE_ENV}`);
+}
+
+// Routes.
+app.use("/api/v1/categories", categoryRoute);
+
+//listen
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`App Running at Port ${PORT}`);
+});
