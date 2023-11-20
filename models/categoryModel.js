6ce312974c7589fe207d4d9064c912f4823c0448
Abdelkader Elsisy <66458422+abdelkader470@users.noreply.github.com>
2023-11-21 00:57:27 +0200

    create category model

---
+++ models/categoryModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+// 1- create Schema
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Category is required"],
+      unique: [true, "Category must be unique"],
+      minLength: [3, "Too short category name"],
+      maxLength: [32, "Too long category name"],
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+    image: String,
+  },
+  { timestamps: true }
+);
+// 2- create Model
+const CategoryModel = mongoose.model("Category", categorySchema);
+
+module.exports = CategoryModel;
