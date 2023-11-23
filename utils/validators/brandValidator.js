aa700e07444c72c0ec0e7877832e588747db9cfd
Abdelkader Elsisy <66458422+abdelkader470@users.noreply.github.com>
2023-11-23 02:50:25 +0200

    create brand vaildator

---
+++ utils/validators/brandValidator.js
@@ -0,0 +1,26 @@
+const { check } = require("express-validator");
+const validatorMiddleware = require("../../middlewares/validatorMiddleware");
+
+exports.getBrandValidator = [
+  check("id").isMongoId().withMessage("Invalid Brand id format"),
+  validatorMiddleware,
+];
+
+exports.CreateBrandValidator = [
+  check("name")
+    .notEmpty()
+    .withMessage("Brand is required")
+    .isLength({ min: 3 })
+    .withMessage("Too short Brand name")
+    .isLength({ max: 32 })
+    .withMessage("Too long Brand name"),
+  validatorMiddleware,
+];
+exports.updateBrandValidator = [
+  check("id").isMongoId().withMessage("Invalid Brand id format"),
+  validatorMiddleware,
+];
+exports.deleteBrandValidator = [
+  check("id").isMongoId().withMessage("Invalid Brand id format"),
+  validatorMiddleware,
+];
