21d99c637f8c97669d611e7c9baea75bc45e57ca
Abdelkader Elsisy <66458422+abdelkader470@users.noreply.github.com>
2023-12-12 14:54:47 +0200

    Calculate Ratings Quantity And Average When Create Review

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Product = require("./productModel");
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -30,5 +31,32 @@ reviewSchema.pre(/^find/, function (next) {
   this.populate({ path: "user", select: "name" });
   next();
 });
-
+reviewSchema.statics.calcAverageRatingsAndQuantity = async function (
+  productId
+) {
+  const result = await this.aggregate([
+    { $match: { product: productId } },
+    {
+      $group: {
+        _id: "product",
+        avgRatings: { $avg: "$ratings" },
+        ratingsQuantity: { $sum: 1 },
+      },
+    },
+  ]);
+  if (result.length > 0) {
+    await Product.findByIdAndUpdate(productId, {
+      ratingsAverage: result[0].avgRatings,
+      ratingsQuantity: result[0].ratingsQuantity,
+    });
+  } else {
+    await Product.findByIdAndUpdate(productId, {
+      ratingsAverage: 0,
+      ratingsQuantity: 0,
+    });
+  }
+};
+reviewSchema.post("save", async function () {
+  await this.constructor.calcAverageRatingsAndQuantity(this.product);
+});
 module.exports = mongoose.model("Review", reviewSchema);
